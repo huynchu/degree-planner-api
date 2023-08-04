481fbc3fee2e35bfe4aee7618d7f1e8cb45239e5
Huy Chu <chuhuyrpi@gmail.com>
2023-08-04 12:38:39 -0400

    project setup

---
+++ cmd/app/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// load config
+	fmt.Println("loading config...")
+
+	// connect to db
+	fmt.Println("connecting to mongodb...")
+
+	// create chi router
+
+}
